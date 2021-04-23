d423b67a4efc0809c5cdb8b5a397b086b7040dc4
jinchen1036 <jinchen1036@users.noreply.github.com>
2021-04-23 18:54:33 -0400

    stratified_sampling_data_by_price_range #17

--- final/general_sampling.py
+++ final/general_sampling.py
@@ -1,5 +1,10 @@
 from final.random_sampling.even_sample_brand import stratified_sampling_by_brand
 from final.random_sampling.even_sample_category import stratified_sampling_by_category
+from final.random_sampling.even_sample_by_price_range import stratified_sampling_by_price
+
+
+price_sample_df = stratified_sampling_by_price(file_dir="../data",number_samples = 50000,
+                                               include_high_price = True, save_sample_df = True)
 
 brand_sample_df = stratified_sampling_by_brand(file_dir="../data",number_samples = 50000,
                                                replace = False, save_sample_df = True)
--- final/random_sampling/even_sample_by_price_range.py
+++ final/random_sampling/even_sample_by_price_range.py
@@ -3,31 +3,40 @@ import pandas as pd
 from datetime import datetime
 
 
+def sampling_price(s,number_sample_per_price,replace,random_num, extract_num = 0):
+    replace_item = False
+
+    if s.iloc[0]['price_bin'] == pd.Interval(left=500, right=2500):
+        replace_item = replace
+    if replace:
+        if s.iloc[0]['price_bin'] == pd.Interval(left=500, right=2500):
+            number_sample_per_price -= 15 * extract_num
+        else:
+            number_sample_per_price +=  extract_num
+
+    return s.sample(number_sample_per_price,
+                    replace = replace_item,random_state=random_num)
 def stratified_sampling_by_price(file_dir="../../data",number_samples = 10000, include_high_price = False, save_sample_df = True):
     random_num = int(datetime.now().timestamp())
+    replace = False
+    extract_num = 0
     # read data
     df = pd.read_csv("%s/data_wo_missing_values_split_category.csv"%file_dir) # result shape: (844460, 10)
 
-    bins = [0, 10, 20, 30, 40,50,60,70,80,90,100, 200,500,1000,2500]
+    bins = [0,5, 10,15, 20, 25, 30, 40,50,60,70,80,90,100, 200,500, 2500] # 17  bins
     df['price_bin'] = pd.cut(df['price'], bins)
-
-
-    brand_dist = df.brand_name.value_counts()
-    limit = 10
-    brand_item_limit_dict = {15000: 50, 50000: 100}
-    for need_sample, item_limit in brand_item_limit_dict.items():
-        if number_samples >= need_sample:
-            limit = item_limit
-    brand_w_over_100_item  = brand_dist[brand_dist>limit].index.to_list()
-    df = df.loc[df.brand_name.isin(brand_w_over_100_item), :] # result shape: (800066, 10)
+    print(df['price_bin'].value_counts())
+    if not include_high_price:
+        df = df.loc[df.price_bin != pd.Interval(left=500, right=2500), :] # result shape: (800066, 10)
+    else:
+        replace = True
+        extract_num = (number_samples -  (17*1000)) // 5000
 
     # random sampling
-    number_sample_per_brand = math.ceil(number_samples / len(brand_w_over_100_item))
-    if number_samples > 55000:
-        number_sample_per_brand += 1
+    number_sample_per_price = math.ceil(number_samples / len(df['price_bin'].value_counts()))
+
     # get sample
-    sampling_df = df.groupby('brand_name').apply(lambda s: s.sample(min(len(s),number_sample_per_brand),
-                                                                    random_state=random_num))
+    sampling_df = df.groupby('price_bin').apply(lambda s: sampling_price(s, number_sample_per_price,replace,random_num,extract_num))
 
     # shuffle rows
     sampling_df = sampling_df.sample(frac=1)
@@ -36,8 +45,9 @@ def stratified_sampling_by_price(file_dir="../../data",number_samples = 10000, i
     if len(sampling_df) >  number_samples:
         sampling_df = sampling_df[:number_samples]
 
-    print(sampling_df.brand_name.value_counts())
+    print(sampling_df['price_bin'].value_counts())
 
     if save_sample_df:
-        sampling_df.to_csv("%s/random_samples/stratified_sampling_data_by_price_sz%d_%d.csv"%(file_dir,len(sampling_df),random_num), index=False)
+        file_name = '_w%shigh_sz%d_%d' % ('' if include_high_price else 'o',len(sampling_df),random_num)
+        sampling_df.to_csv("%s/random_samples/stratified_sampling_data_by_price%s.csv"%(file_dir,file_name), index=False)
     return sampling_df
