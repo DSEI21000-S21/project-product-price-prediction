ab318c24ce04a93e77f79c62cb55352037ac826a
jinchen1036 <jinchen1036@users.noreply.github.com>
2021-05-07 23:11:48 -0400

    fix preprocessing bug

--- final/data_process/format_missing_data.py
+++ final/data_process/format_missing_data.py
@@ -2,13 +2,16 @@
 def separate_categories_replace_nan_w_missing(df):
     cat_columns = df.category_name.str.split('/')
     for i in range(3):
-        df['category_%d'%(i+1)] = cat_columns.str.get(0)
+        df['c%d'%(i+1)] = cat_columns.str.get(0)
     df.drop('category_name', axis=1, inplace=True)
     return df
 
 
 def fill_na_with_missing(df):
     for column in df.columns:
-        df[column].fillna('missing', inplace=True)
+        if column == "item_description":
+            df[column].fillna('No description yet', inplace=True)
+        else:
+            df[column].fillna('missing', inplace=True)
     return df
 
